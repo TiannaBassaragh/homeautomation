8c717ccc0bb31608e01163c12938ddce3c72e7af
Tianna <151879019+TiannaBassaragh@users.noreply.github.com>
2025-03-26 23:58:03 -0500

    Update hardware_wamos.ino

--- hardware_wamos/hardware_wamos.ino
+++ hardware_wamos/hardware_wamos.ino
@@ -1,19 +1,28 @@
 
 #include <SoftwareSerial.h>
 // IMPORT ALL REQUIRED LIBRARIES
+#include <Arduino.h> //arduino functions like digitalWrite and pinMode
+
+#include <stdlib.h> //standard library functions in c
+
+#include <stdio.h> //standard i/o functions like printf
+
+#include <ArduinoJson.h> //for json messages
+
+#include <NewPing.h> //for ultrasonic sensor
 
 #include <math.h>
    
 //**********ENTER IP ADDRESS OF SERVER******************//
 
-#define HOST_IP     "localhost"       // REPLACE WITH IP ADDRESS OF SERVER ( IP ADDRESS OF COMPUTER THE BACKEND IS RUNNING ON) 
+#define HOST_IP     "www.yanacreations.com"       // REPLACE WITH IP ADDRESS OF SERVER ( IP ADDRESS OF COMPUTER THE BACKEND IS RUNNING ON) 
 #define HOST_PORT   "8080"            // REPLACE WITH SERVER PORT (BACKEND FLASK API PORT)
 #define route       "api/update"      // LEAVE UNCHANGED 
-#define idNumber    "620012345"       // REPLACE WITH YOUR ID NUMBER 
+#define idNumber    "620162297"       // REPLACE WITH YOUR ID NUMBER 
 
 // WIFI CREDENTIALS
-#define SSID        "YOUR WIFI"      // "REPLACE WITH YOUR WIFI's SSID"   
-#define password    "YOUR PASSWORD"  // "REPLACE WITH YOUR WiFi's PASSWORD" 
+#define SSID        "MonaConnect"     // "REPLACE WITH YOUR WIFI's SSID"   
+#define password    ""  // "REPLACE WITH YOUR WiFi's PASSWORD" 
 
 #define stay        100
  
@@ -24,30 +33,76 @@
 #define espTX         11
 #define espTimeout_ms 300
 
- 
+#define trigPin 13 //pin to output to trigger pin on sensor
+#define echoPin 12 //pin to send info to echo pin on sensor
+#define maxDistance 240 //maximum distance for ultrasonic sensor in cm
+
+#define maxWaterheight 77.763 //max waterheight in inches (where the tank is at 100% capacity)
+// #define maxReserveVol 1000 //US Gallons when the tank is at 100% capacity
+#define sensorHeight 94.5 //inches from base of tank
+#define tankDiameter 61.5 //inches
+
+/*Variables*/
+float  radar        =0; //distance in inches
+double waterheight  =0; //inches
+double reserve      =0; //volume filled in reserve in US Gallons
+double percentage   =0; //percent of tank filled
  
 /* Declare your functions below */
- 
- 
+void espSend(char command[]);
+void espUpdate(char mssg[]);
+void espInit();
+double calcWaterheight(double); //metres
+double calcReserveVol(double); //unit US gallons
+double calcPercentage(); //percentage using waterheight and max water level
 
 SoftwareSerial esp(espRX, espTX); 
  
+NewPing sonar(trigPin, echoPin, maxDistance); //setup for sensor
 
 void setup(){
 
   Serial.begin(115200); 
   // Configure GPIO pins here
-
+  //Since Im using NewPing I don't have any to configure
  
-
   espInit();  
  
 }
 
-void loop(){ 
-   
-  // send updates with schema ‘{"id": "student_id", "type": "ultrasonic", "radar": 0, "waterheight": 0, "reserve": 0, "percentage": 0}’
+void loop(){
+  
+  radar = sonar.ping_in(); //Continuously measure the distance between the sensor and the water in the tank. This must be considered as the ‘radar’ measurement
 
+  waterheight = calcWaterheight(radar); //Using the radar measurement, calculate the water level/height in the tank. This must be considered as the ‘waterheight’ measurement.
+  reserve = calcReserveVol(waterheight); //Calculate remaining water reserves, in US Gallons, from the radar measurement. Consider this as the ‘reserve’ measurement. That is, compute the volume of water currently in the tank with units of US Gallons.
+  percentage = calcPercentage(waterheight); // Compute a percentage using the waterheight and the Maximum water level of the tank
+  
+  // Serial.print(radar);
+  // Serial.println(" inches");
+  // Serial.print(waterheight);
+  // Serial.println(" inches");
+  // Serial.print(reserve);
+  // Serial.println(" gallons");
+  // Serial.print(percentage);
+  // Serial.println("%");
+  // Serial.println("");
+
+  delay(50);
+
+  // send updates with schema ‘{"id": "student_id", "type": "ultrasonic", "radar": 0, "waterheight": 0, "reserve": 0, "percentage": 0}’
+  JsonDocument doc;
+  char message[290]     = {0};
+  doc["id"]             = idNumber;
+  doc["type"]           = "ultrasonic";
+  doc["radar"]          = radar;
+  doc["waterheight"]    = waterheight;
+  doc["reserve"]        = reserve;
+  doc["percentage"]     = percentage;
+
+  serializeJson(doc, message);  // Seralize / Covert JSon object to JSon string and store in char* array
+
+  espUpdate(message);
 
 
   delay(1000);  
@@ -86,7 +141,7 @@ void espUpdate(char mssg[]){
 void espInit(){
     char connection[100] = {0};
     esp.begin(115200); 
-    Serial.println("Initiallizing");
+    Serial.println("Initializing"); //the word initializing was spelt wrong
     esp.println("AT"); 
     delay(1000);
     esp.println("AT+CWMODE=1");
@@ -105,5 +160,27 @@ void espInit(){
 }
 
 //***** Design and implement all util functions below ******
- 
+double calcWaterheight(double radar){
+  // Ensure radar is within valid bounds
+  if (radar < 0) {
+    radar = 0;
+  } else if (radar > sensorHeight + maxWaterheight) {
+    radar = sensorHeight + maxWaterheight;
+  }
 
+  // Calculate the water height
+  return sensorHeight + maxWaterheight - radar; //to account for space between max waterheight and sensor
+  //ignoring the space we would have: 
+  //return maxWaterheight - radar;
+}
+
+double calcReserveVol(double waterheight){
+  //formula: (pi*(r^2)*h) / (231 to get US Gallons from Cubic inches)
+  //Calculate remaining water reserves, in US Gallons, from the radar measurement. That is, compute the volume of water currently in the tank with units of US Gallons.
+  float tankRadius = tankDiameter/2;
+  return (3.1415*tankRadius*tankRadius*waterheight) / 231.0;
+}
+double calcPercentage(double waterheight){
+  //Compute a percentage using the waterheight and the Maximum water level of the tank.
+  return (waterheight/maxWaterheight)*100;
+}
